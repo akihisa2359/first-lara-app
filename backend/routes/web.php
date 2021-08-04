1e3e550b34cc28a6ebc35203e281ef9b42b0f257
akihisa.makimoto <akihisa.makimoto@localadminnoMacBook-Pro-2.local>
2021-08-04 21:04:30 +0900

    投稿一覧ページ実装

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -23,6 +23,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::get('/posts', [App\Http\Controllers\PostController::class, 'index']);
 Route::post('/post', [App\Http\Controllers\PostController::class, 'store']);
 
 // Route::get('/create', [App\Http\Controllers\HomeController::class, 'create']);
